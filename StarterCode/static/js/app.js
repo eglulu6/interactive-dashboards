b5dbae1c7ec694869eb57a4a0d0eac217dae7b54
Emerald Desktop <eglulu6@gmail.com>
2021-04-08 21:59:52 -0400

    adding code for bubbles, gauge, and meta

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,10 +1,11 @@
-// Fetch data from JSON file referred to as importedData
+
+/****************** read in json file ***********************/
 d3.json("../../../data/samples.json").then((importedData) =>
 {
     var samplesData = importedData;
     console.log(samplesData)
 
-/****************** Make the dropdown of IDs***********************/
+    /****************** Make the dropdown of IDs ***********************/
     
     // identify dropdown box in html file
     var dropdown = d3.select("#selDataset")
@@ -19,12 +20,13 @@ d3.json("../../../data/samples.json").then((importedData) =>
             dropdown.append('option').text(element)
         });
 
-/*****************Filter data by selected ID***************************************/
+    /***************** Filter data by selected ID ***************************************/
         
     //select the dropdown and set an action on event
     dropdown.on("change", handleSelect);
 
-/************************Define change event actions*********************/
+    /************************ Define change event actions *********************/
+
     //define the actions to take on event
     function handleSelect(event) 
     {
@@ -32,11 +34,13 @@ d3.json("../../../data/samples.json").then((importedData) =>
         var selectedID = dropdown.property("value");
         console.log(selectedID)
 
-        /*****************filter all data by dropdown value**************/ 
+        /***************** filter all data by dropdown value **************/ 
+
         var filteredIDInfo = samplesData[0].samples.filter(item => item.id === selectedID)
         console.log(filteredIDInfo)
 
-        /*****************slice for top 10*************************/
+        /***************** slice for top 10 *************************/
+
         var top10_otu_ids = []
         filteredIDInfo[0].otu_ids.slice(0,10).forEach(element => 
             {
@@ -44,25 +48,11 @@ d3.json("../../../data/samples.json").then((importedData) =>
             )});
         var top10_sample_values = filteredIDInfo[0].sample_values.slice(0,10);
         var top10_otu_labels = filteredIDInfo[0].otu_labels.slice(0,10);
-        var color = [rgb(252, 101, 0), rgb(), rgb(), rgb(68, 184, 39), rgb(39, 184, 167), rgb(252, 223, 0),rgb(), rgb(), rgb(252, 0, 0), rgb(0, 76, 255)]
-        
-// copied code for auto gen RGB colors
-        // function getRandomRgb() 
-        // {
-        //     var num = Math.round(0xffffff * Math.random());
-        //     var r = num >> 16;
-        //     var g = num >> 8 & 255;
-        //     var b = num & 255;
-        //     return 'rgb(' + r + ', ' + g + ', ' + b + ')';
-        //   }
-        // for (var i = 0; i < 10; i++) 
-        // {
-        //     console.log(getRandomRgb());
-        // }
+        var top10_colors = ['rgb(252, 101, 0, .5)', 'rgb(117, 77, 43, .5)', 'rgb(255, 48, 165, .5)', 'rgb(68, 184, 39, .5)', 'rgb(39, 184, 167, .5)', 'rgb(252, 223, 0, .5)', 'rgb(0, 0, 0, .9)', 'rgb(118, 0, 173, .5)', 'rgb(252, 0, 0, .5)', 'rgb(0, 76, 255, .5)']
 
         console.log(top10_otu_ids)// check slice
 
-        /*****************Ploting h-bar chart**************************/
+        /***************** Ploting h-bar chart **************************/
 
         var bar_data = 
         [{
@@ -71,64 +61,121 @@ d3.json("../../../data/samples.json").then((importedData) =>
             y: top10_otu_ids,
             orientation: 'h',
             text: top10_otu_labels,
-            mode: 'markers'
-        }]; 
+            mode: 'markers',
+            marker:
+            {
+                color: all_sample_values,
+                colorscale: [[0, 'rgb(23, 212, 30)'], [1, 'rgb(32, 23, 212)']],
+                opacity: 0.6,
+            }
+        }]; // bar_data end
 
         var bar_layout = 
         {
-            yaxis: {autorange: 'reversed'}
-        };
+            yaxis: {autorange: 'reversed'},
+            opacity: 0.6,
+            title: "Top 10 OTUs"
+        }; // bar_layout end
+
         Plotly.newPlot('bar', bar_data, bar_layout);
 
-        /*****************Ploting bubble chart**************************/
+        /****************** grab all values for bubble chart ***************/
+
+        var all_otu_ids = filteredIDInfo[0].otu_ids;
+        console.log(all_otu_ids);
+
+        var all_otu_ids_txt = []
+            filteredIDInfo[0].otu_ids.forEach(element => 
+                {
+                    var substring = all_otu_ids_txt.push(`OTU ${element}`
+                )});
+        console.log(all_otu_ids_txt);
+
+        var all_sample_values = filteredIDInfo[0].sample_values;
+        console.log(all_sample_values)
+
+        var all_otu_labels = filteredIDInfo[0].otu_labels;
+        console.log(all_otu_labels)
+
+        /***************** Ploting bubble chart **************************/  
+        
+        console.log(all_sample_values)
+
         var bubble_data = 
         [{
-            x: top10_otu_ids,
-            y: top10_sample_values,
+            x: all_otu_ids,
+            y: all_sample_values,
             mode: 'markers',
-            text: top10_otu_labels,
+            text: all_otu_labels,
             marker: 
             {
-              size: top10_sample_values,
-              color: top10_otu_ids
+              size: all_sample_values,
+              opacity: 0.6,
+              color: all_sample_values,
+              colorscale: [[0, 'rgb(23, 212, 30)'], [1, 'rgb(32, 23, 212)']]
             }
-        }];
+        }]; // bubble_data end
           
         var bubble_layout = 
           {
-            title: 'Marker Size',
+            title: 'Sample Values',
             showlegend: false,
-            height: 600,
+            height: 1200,
             width: 1200,
-            xaxis: {title: top10_otu_ids},
-            opacity: 0.6
-          };
-          
+            xaxis: 
+            {
+                title: all_otu_ids
+            },
+          }; // bubble_layout end
         Plotly.newPlot('bubble',bubble_data, bubble_layout);
+
+        /***************** creating gauge **************************/
+
+        var filteredMetaInfo = samplesData[0].metadata.filter(item => item.id === parseInt(selectedID))
+        let washing = filteredMetaInfo[0].wfreq
+        console.log(washing)
+
+        var data = 
+        [
+            {
+                domain: { x: [0, 1], y: [0, 1] },
+                value: washing,
+                title: { text: "Washing Frequency" },
+                type: "indicator",
+                mode: "gauge+number"
+            }
+        ];
+        
+        var layout = 
+        { 
+            width: 600, 
+            height: 500, 
+            margin: 
+                { 
+                    t: 0, 
+                    b: 9 
+                } 
+        };
+        Plotly.newPlot('gauge', data, layout);
+
+        /***************** creating demographics table **************************/
+
+        console.log(filteredMetaInfo)
+
+        var demographics = d3.select('#sample-metadata')
+        demographics.append('p').text(`ID: ${filteredMetaInfo[0].id}`);
+        demographics.append('p').text(`Ethnicity: ${filteredMetaInfo[0].ethnicity}`);
+        demographics.append('p').text(`Gender: ${filteredMetaInfo[0].gender}`);
+        demographics.append('p').text(`Age: ${filteredMetaInfo[0].age}`);
+        demographics.append('p').text(`Location: ${filteredMetaInfo[0].location}`);
+        demographics.append('p').text(`BBtype: ${filteredMetaInfo[0].bbtype}`);
+        demographics.append('p').text(`Wfreq: ${filteredMetaInfo[0].wfreq}`);
+
+
     } //handleEvent Function end
 
+/*********************** End Event Actions ***********************************/
 
-})
+})// .then funtion end
 
-
-
-        // // create a list to hold mini dicts for each corresponding values in otu_ids, sample_values, otu_labels
-        // var samples_list = [] //#944 === 28 array values
-        // for (let index = 0; index < filteredIDInfo[0].sample_values.length; index++) 
-        //     // const element = array[index];
-        //     {
-        //         samples_list.push({'otu_ids': filteredIDInfo[0].otu_ids[index], 'sample_values': filteredIDInfo[0].sample_values[index], 'otu_labels': filteredIDInfo[0].otu_labels[index] });
-        //     }
-        //     console.log(samples_list)
-
-        
-
-        // // sort filteredIDInfo by teh sample_value field in descending order
-        // // var sample_values = filteredIDInfo[0].sample_value
-        // samples_list.sort((first , second) => 
-        // {
-        //     // sorting in descending order
-        //     return second.sample_values - first.sample_values;
-        // }); // sort funciton End
-        // console.log(samples_list);
-
+/********************************* END ****************************************/
